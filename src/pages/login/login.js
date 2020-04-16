84ab20c3e299a0b598410e698f7bf6c81439b940
baltazarparra <baltazarparra@outlook.com>
2020-04-16 15:34:04 -0300

    Chore: add login feature

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import { Button } from '@material-ui/core'
 import { ReactComponent as Logo } from './logo.svg'
 
-import styled from 'styled-components'
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPOdSWubmp9q7OxceTwoWdFoNGQRxKJ2g',
+  authDomain: 'deliveryapp-4db44.firebaseapp.com',
+  databaseURL: 'https://deliveryapp-4db44.firebaseio.com',
+  projectId: 'deliveryapp-4db44',
+  storageBucket: 'deliveryapp-4db44.appspot.com',
+  messagingSenderId: '746638879646',
+  appId: '1:746638879646:web:6700eda06f57e341cb048f',
+  measurementId: 'G-NYP8VYZF1L'
+}
+
+firebase.initializeApp(firebaseConfig)
 
 const Container = styled.main`
   display: flex;
@@ -20,14 +34,25 @@ const LoginButton = styled(Button)`
   margin: 1em;
 `
 
-const Login = () => (
-  <Container>
-    <Title>
-      Delivery App
-    </Title>
-    <Logo />
-    <LoginButton>Entrar com github</LoginButton>
-  </Container>
-)
+const Login = () => {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) console.log(user)
+    })
+  }, [])
+  return (
+    <Container>
+      <Title>
+        Delivery App
+      </Title>
+      <Logo />
+      <LoginButton onClick={() => {
+        const provider = new firebase.auth.GithubAuthProvider()
+        firebase.auth().signInWithRedirect(provider)
+      }}
+      >Entrar com github</LoginButton>
+    </Container>
+  )
+}
 
 export default Login
