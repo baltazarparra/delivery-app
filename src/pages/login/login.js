5944231cbb6d6721a5afb4e74d4bd6b6792c7c1b
baltazarparra <baltazarparra@outlook.com>
2020-04-16 11:15:01 -0300

    Chore: login page markup

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import { ReactComponent as Logo } from './logo.svg'
+
+import styled from 'styled-components'
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+
+const Title = styled.h1`
+  font-size: 2em;
+`
+
+const LoginButton = styled(Button)`
+  margin: 1em;
+`
 
 const Login = () => (
-  <h1>Login</h1>
+  <Container>
+    <Title>
+      Delivery App
+    </Title>
+    <Logo />
+    <LoginButton>Entrar com github</LoginButton>
+  </Container>
 )
 
 export default Login
